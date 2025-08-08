bec041d72ac364240a03057f630f104a6892ba7a
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-08-08 09:39:07 -0300

    feat: aprendento serializadores

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,12 +1,23 @@
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import Group, User, Permission
 from rest_framework import serializers
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ['id', 'name']
+ 
+        
+class GroupSerializer(serializers.ModelSerializer):
+    permissions = PermissionSerializer(many=True)
+    class Meta:
+        model = Group
+        fields = ['id', 'name', 'permissions'] 
+
+
+class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True)
+    permissions = PermissionSerializer(many=True)
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
-        
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
