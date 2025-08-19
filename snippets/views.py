f8a4b4c96cdb69a7d45d0779f26bf6cb9cfe5d02
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-08-19 10:39:30 -0300

    feat: add login to the browsable API

--- snippets/views.py
+++ snippets/views.py
@@ -6,14 +6,20 @@ from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+    
               
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
