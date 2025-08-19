c16a496d18e8c5bedbed511a79c39ce6a7045865
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-08-19 11:03:18 -0300

    feat: Authentication e permissions

--- snippets/views.py
+++ snippets/views.py
@@ -7,6 +7,7 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -19,7 +20,7 @@ class SnippetList(generics.ListCreateAPIView):
     
               
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
